cd7b0c8b50bcbeca0261c900924d71df01dceb2f
Trojan <trojan@pop-os.localdomain>
2022-11-21 12:07:27 -0300

    Move lines keymap fixed

--- lua/globals/keymaps.lua
+++ lua/globals/keymaps.lua
@@ -29,8 +29,8 @@ keymap("n", "<S-Down>", ":m .+1<CR>", opts)
 keymap("v", "<S-Up>", ":m .-2<CR>", opts)
 keymap("v", "<S-Down>", ":m .+1<CR>", opts)
 
-keymap("n", "<C-j>", ":m .-2<CR>", opts)
-keymap("n", "<C-k>", ":m .+1<CR>", opts)
+keymap("n", "<C-k>", ":m .-2<CR>", opts)
+keymap("n", "<C-j>", ":m .+1<CR>", opts)
 
 -- Quick Exit Insert Mode
 keymap("i", "jk", "<ESC>", opts)
